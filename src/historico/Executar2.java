81bce36d9d1ddafe71dee0c2685b1ad9b87ed9a4
mateus-hub <theusskt2018@gmail.com>
2024-01-22 16:13:16 -0300

    Classe Individuo 2

--- src/historico/Executar2.java
+++ src/historico/Executar2.java
@@ -1,9 +1,9 @@
-package algoritmo;
+package historico;
 
 import java.util.ArrayList;
 import java.util.List;
 
-class Produto {
+/*class Produto {
 	private String nome;
 	private Double espaco;
 	private Double valor;
@@ -113,8 +113,10 @@ class Individuo {
 	}
 }
 
-public class Executar {
+public class Executar2 {
 	public static void main(String args[]) {
+		//Produto p1 = new Produto("Geladeira Dako", 0.751, 999.90);
+		//System.out.println(p1.getNome() + " " + p1.getEspaco() + " " + p1.getValor());
 		
 		List<Produto> listaProdutos = new ArrayList<>();
 		listaProdutos.add(new Produto("Geladeira Dako", 0.751, 999.90));
@@ -131,27 +133,9 @@ public class Executar {
 		listaProdutos.add(new Produto("Geladeira Consul", 0.870, 1199.89));
 		listaProdutos.add(new Produto("Notebook Lenovo", 0.498, 1999.90));
 		listaProdutos.add(new Produto("Notebook Asus", 0.527, 3999.00));
-
-		List espacos = new ArrayList<>();
-		List valores = new ArrayList<>();
-		List nomes = new ArrayList<>();
-	    for (Produto produto: listaProdutos) {
-	    	espacos.add(produto.getEspaco());
-	    	valores.add(produto.getNome());
-	    	nomes.add(produto.getNome());
-	    }
-	    Double limite = 3.0;
-	    
-	    Individuo individuo1 = new Individuo(espacos, valores, limite);
-	    System.out.println("Espaços: " + individuo1.getEspacos());
-	    System.out.println("Valores: " + individuo1.getValores());
-	    System.out.println("Cromossomo: " + individuo1.getCromossomo());
-	    System.out.println("\nComponentes da carga");
-	    for (int i = 0; i < listaProdutos.size(); i++) {
-	    	if (individuo1.getCromossomo().get(i) == "1") {
-	    		System.out.println("Nome: " + listaProdutos.get(i).getNome() 
-	    				+ " R$: " + listaProdutos.get(i).getValor());
-	    	}
-	    }
+		for (Produto produto : listaProdutos) {
+			System.out.println(produto.getNome());
+		}
 	}
-}
+
+}*/
