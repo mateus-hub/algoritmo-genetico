1047da47080945a399c4b99370a5cfdff037c6f4
mateus-hub <theusskt2018@gmail.com>
2024-01-22 15:22:11 -0300

    Classe Individuo 1

--- src/historico/Executar1.java
+++ src/historico/Executar1.java
@@ -1,4 +1,4 @@
-package algoritmo;
+package historico;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,81 +39,7 @@ class Produto {
 	}
 }
 
-class Individuo {
-	private List espacos = new ArrayList<>();
-	private List valores = new ArrayList<>();
-	private Double limiteEspacos;
-	private Double notaAvaliacao;
-	private int geracao;
-	private List cromossomo = new ArrayList<>();
-	
-	public Individuo(List espacos, List valores, Double limiteEspacos) {
-		this.espacos = espacos;
-		this.valores = valores;
-		this.limiteEspacos = limiteEspacos;
-		this.notaAvaliacao = 0.0;
-		this.geracao = 0;
-		
-		for (int i = 0; i < this.espacos.size(); i++) {
-			if (java.lang.Math.random() < 0.5) {
-				this.cromossomo.add("0");
-				
-			} else {
-				this.cromossomo.add("1");
-			}
-		}
-	}
-
-	public List getEspacos() {
-		return espacos;
-	}
-
-	public void setEspacos(List espacos) {
-		this.espacos = espacos;
-	}
-
-	public List getValores() {
-		return valores;
-	}
-
-	public void setValores(List valores) {
-		this.valores = valores;
-	}
-
-	public Double getLimiteEspacos() {
-		return limiteEspacos;
-	}
-
-	public void setLimiteEspacos(Double limiteEspacos) {
-		this.limiteEspacos = limiteEspacos;
-	}
-
-	public Double getNotaAvaliacao() {
-		return notaAvaliacao;
-	}
-
-	public void setNotaAvaliacao(Double notaAvaliacao) {
-		this.notaAvaliacao = notaAvaliacao;
-	}
-
-	public int getGeracao() {
-		return geracao;
-	}
-
-	public void setGeracao(int geracao) {
-		this.geracao = geracao;
-	}
-
-	public List getCromossomo() {
-		return cromossomo;
-	}
-
-	public void setCromossomo(List cromossomo) {
-		this.cromossomo = cromossomo;
-	}
-}
-
-public class Executar {
+public class Executar1 {
 	public static void main(String args[]) {
 		//Produto p1 = new Produto("Geladeira Dako", 0.751, 999.90);
 		//System.out.println(p1.getNome() + " " + p1.getEspaco() + " " + p1.getValor());
